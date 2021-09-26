747fec450091140706364bfafbdb03d69f886d17
Mohammed Javed <javedamaan21@hotmail.com>
2021-09-26 12:49:32 +0100

    Second Commit

---
+++ LeetCode.Problems.Solution/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace LeetCode.Problems.Solution
+{
+    class Progrnam
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 2, 5, 7, 8, 93, 4, 5, 67, 3, 6 };
+            int index = 2;
+            PrintArray(nums);
+            Console.WriteLine($"The {index} Highest  Number in the array is {GetHighestNumberFromArray(nums, index)}");
+        }
+
+        public static void PrintArray(int[] nums)
+        {
+            Console.WriteLine();
+            Array.ForEach(nums, x => Console.Write(" " + x));
+            Console.WriteLine();
+
+        }
+        public static int GetHighestNumberFromArray(int[] nums, int index)
+        {
+            //Sorting the array
+            Array.Sort(nums);
+            PrintArray(nums);
+
+            //Reverse the sorted array
+            Array.Reverse(nums);
+            PrintArray(nums);
+
+            return nums[index];
+        }
+
+
+    }
+}
